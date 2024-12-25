9a216d2207732982bfdd61c5f6b4a3a902606e95
atishaytuli07 <atishaytuliiaf@gmail.com>
2024-12-25 13:01:14 +0000

    25.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 function App() {
   return (
-    <main className="bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold">Hello World</h1>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero />
     </main>
   )
 }
