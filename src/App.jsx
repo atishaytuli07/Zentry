66c7c64df2ed401f7bd28f98d1266ffa20d1acd5
atishaytuli07 <atishaytuliiaf@gmail.com>
2024-12-25 10:29:52 +0000

    25.12

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main className="bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </main>
+  )
+}
+
+export default App;
